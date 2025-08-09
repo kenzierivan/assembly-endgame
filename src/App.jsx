397070fd41b9a89a705fb88d58ae8792a7be5540
Kenzie Rivan <kenzierivan261@gmail.com>
2025-08-09 22:13:21 +0800

    Added a game status section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import Header from "./components/Header"
+
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+      <Header />
+      <section className="game-status">
+        <h1>You win!</h1>
+        <p>Well done🎉</p>
+      </section>
+    </main>
   )
 }
 export default App
