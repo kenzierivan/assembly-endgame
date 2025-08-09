a6e3e9267222abe26f3c0edaaf472ca41a30f5d6
Kenzie Rivan <kenzierivan261@gmail.com>
2025-08-09 23:52:10 +0800

    added language chips

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import Header from "./components/Header"
+import { languages } from "./languages"
 
 const App = () => {
+  const languageElement = languages.map(language => {
+    const styles = {
+      backgroundColor: language.backgroundColor,
+      color: language.color,
+    };
+    return (
+      <span key={language.name} style={styles}>{language.name}</span>
+    )
+  })
+
   return (
     <main>
       <Header />
       <section className="game-status">
         <h1>You win!</h1>
         <p>Well done🎉</p>
+      </section>
+      <section className="languages-chips">
+        {languageElement}
       </section>
     </main>
   )
