e9ed4ded8573f051751de427699c50d69cf1d33a
Kenzie Rivan <kenzierivan261@gmail.com>
2025-08-10 00:27:02 +0800

    Change background color of keyboard based on correct or incorrect guesses

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,16 @@ import Header from "./components/Header"
 import { languages } from "./languages"
 
 const App = () => {
+  //State values
+  const [currentWord, setCurrentWord] = useState("react")
+  const [guessedLetters, setGuessedLetters] = useState([]);
+
+  //Derived values
+  const wrongGuessCount = guessedLetters.filter(letter => !currentWord.includes(letter)).length;
+
+  //Static values
+  const alphabets = "abcdefghijklmnopqrstuvwxyz";
+
   const languageElement = languages.map(language => {
     const styles = {
       backgroundColor: language.backgroundColor,
@@ -13,17 +23,26 @@ const App = () => {
     )
   })
 
-  const [currentWord, setCurrentWord] = useState("react")
+
   const wordElement = currentWord.split("").map((letter, index) => {
+    const isGuessed = guessedLetters.includes(letter);
     return (
-      <span className="letter-box" key={index}>{letter.toUpperCase()}</span>
+      <span className="letter-box" key={index}>{isGuessed ? letter.toUpperCase() : ""}</span>
     )
   })
 
-  const alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  function guessLetter(letter) {
+    setGuessedLetters(prev => prev.includes(letter) ? prev : [...prev, letter]);
+  }
   const alphabetElement = alphabets.split("").map(letter => {
+    const isGuessed = guessedLetters.includes(letter);
+    const isCorrect = currentWord.includes(letter);
+    const styles = {
+      backgroundColor: isGuessed ? (isCorrect ? "#10A95B" : "#ec664eff") : "#E0E0E0",
+    }
     return (
-      <button key={letter}>{letter}</button>
+      <button style={styles} onClick={() => guessLetter(letter)} key={letter}>{letter.toUpperCase()}</button>
     )
   })
 
